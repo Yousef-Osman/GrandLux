0932aa1c7c5ff795317961e39d7b5d5b13a20ea7
Yousef Osman <yousef.osman.93@outlook.com>
2020-06-29 04:43:50 +0200

    Updated MVC UI

--- GrandLux/Startup.cs
+++ GrandLux/Startup.cs
@@ -26,7 +26,6 @@ namespace GrandLux
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IRoomRepository, RoomRepository>();
